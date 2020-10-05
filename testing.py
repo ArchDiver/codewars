ac2a5410337025d972da5ee423a394ecc69866fc
LSC <lelandscrawford@gmail.com>
2020-10-05 09:54:33 -0400

    oct 5-2

--- testing.py
+++ testing.py
@@ -1136,8 +1136,36 @@ def solve(arr):
 
 
 # # ---------------------------------------------------------------------------------------------
+# In this Kata, you will be given a series of times at which an alarm goes off. Your task will be to determine the maximum time interval between alarms. Each alarm starts ringing at the beginning of the corresponding minute and rings for exactly one minute. The times in the array are not in chronological order. Ignore duplicate times, if any.
+
+# For example:
+# solve(["14:51"]) = "23:59". If the alarm goes off now, it will not go off for another 23 hours and 59 minutes.
+# solve(["23:00","04:22","18:05","06:24"]) == "11:40". The max interval that the alarm will not go off is 11 hours and 40 minutes.
+# In the second example, the alarm goes off 4 times in a day.
+
+# More examples in test cases. Good luck!
+# mine
+from datetime
+def solve(arr):
+    if len(arr) < 1:
+        return "There are no alarms set."
+    if len(arr) < 2:
+        return "23:59"
+    sleep = ''
+    arr = sorted(arr)
+    app.append((datetime.timedetla(
+    for y in range(len(arr)-1):
+        a = datetime.strptime(arr[y],'%I:%M)
+        b = datetime.strptime(arr[y+1], '%I:%M)
+        nap = b - a
+        if nap > sleep:
+            sleep = nap    
+    return sleep
 
 
+
+print(solve(["21:14", "15:34", "14:51", "06:25", "15:30"]))
+print(solve(["14:51"]))
 # # ---------------------------------------------------------------------------------------------
 
 # # ---------------------------------------------------------------------------------------------
