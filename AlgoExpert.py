19a2e5c6af99fbd5412245631816b1eba1ab2d8f
LSC <lelandscrawford@gmail.com>
2021-01-27 07:29:12 -0500

    adding to Algo

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -58,11 +58,11 @@ def closest(bst, t, c):
 # # Branch Sums---------------------------------------------------------------------------------------------
 # This is the class input root. Do not edit it.
 #Only if you are inporting the tree
-class BinaryTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+# class BinaryTree:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
 
 def branchSums(root):
     #write your code here.
