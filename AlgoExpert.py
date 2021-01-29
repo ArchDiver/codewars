90a9d59055a61732d508d98d5cbdb21088d7bb8c
LSC <lelandscrawford@gmail.com>
2021-01-28 21:48:05 -0500

    adding to Algo

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -113,7 +113,22 @@ def nodeDepths(root):
 #         self.right = None
 
 # # Depth-first search---------------------------------------------------------------------------------------------
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
 
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # Write your code here.
+        pass
 
 # # ---------------------------------------------------------------------------------------------
 
