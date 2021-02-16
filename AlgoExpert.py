4fa12bd72847ce5833725d6e3e35c4335e243370
LSC <lelandscrawford@gmail.com>
2021-02-15 21:42:44 -0500

    adding to algo

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -322,7 +322,26 @@
 #     return win
 
 
-# # ---------------------------------------------------------------------------------------------
+# # Non-Constructible Change---------------------------------------------------------------------------------------------
+# Mine that didn't work and would have been inefficiant
+def nonConstructibleChange(coins):
+    c = sorted(coins)
+	nope = False
+	val = 1
+	while not nope:
+		if val in c or check(c, val):
+			val += 1
+		else:
+			nope = True
+    return val
+
+def check(c, val):
+	total = c[0]
+	count = 1
+	while total < val:
+		total += c[count]
+		count += 1
+	return True if total == val else False
 
 # # ---------------------------------------------------------------------------------------------
 
