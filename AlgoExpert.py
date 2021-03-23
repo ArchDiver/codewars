608ee6f478b7eee56f32499211d851f95c12ee7d
LSC <lelandscrawford@gmail.com>
2021-03-22 21:09:58 -0400

    algo

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -432,6 +432,19 @@
 #     return squareArray
 
 # # ---------------------------------------------------------------------------------------------
+# # Insertion Sort
+
+def insertionSort(array):
+    # Write your code here.
+    for i in range(1,len(array)):
+        j = i 
+        while j > 0 and array[j] < array[j - 1]:
+            array[j-1], array[j] =  array[j], array[j-1]
+            j -= 1
+    return array
+    
+array = [8, 5, 2, 9, 5, 6, 3]
+print(insertionSort(array))
 
 # # ---------------------------------------------------------------------------------------------
 
