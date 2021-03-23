1885a82a44391ed880a560e43bfef0502fef64c0
LSC <lelandscrawford@gmail.com>
2021-03-22 21:31:46 -0400

    Insertion Sort

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -434,17 +434,17 @@
 # # ---------------------------------------------------------------------------------------------
 # # Insertion Sort
 
-def insertionSort(array):
-    # Write your code here.
-    for i in range(1,len(array)):
-        j = i 
-        while j > 0 and array[j] < array[j - 1]:
-            array[j-1], array[j] =  array[j], array[j-1]
-            j -= 1
-    return array
+# def insertionSort(array):
+#     # Write your code here.
+#     for i in range(1,len(array)):
+#         j = i 
+#         while j > 0 and array[j] < array[j - 1]:
+#             array[j-1], array[j] =  array[j], array[j-1]
+#             j -= 1
+#     return array
     
-array = [8, 5, 2, 9, 5, 6, 3]
-print(insertionSort(array))
+# array = [8, 5, 2, 9, 5, 6, 3]
+# print(insertionSort(array))
 
 # # ---------------------------------------------------------------------------------------------
 # # Insertion sort
