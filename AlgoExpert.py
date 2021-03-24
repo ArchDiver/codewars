2534466f56873671abef5a78a294c4dfedc1f3bb
LSC <lelandscrawford@gmail.com>
2021-03-23 21:41:01 -0400

    palindrome check- Algo

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -491,12 +491,19 @@
 #         revChar.append(string[i])
 #     return string == "".join(revChar)
 
+# # time O(n) space O(n)
 def isPalindrome(string, idx = 0):
     j = len(string) -1 - idx
-    return True if idx >= j else string[idx] == string[j] and isPalindrome(string, i + 1)
+    return True if idx >= j else string[idx] == string[j] and isPalindrome(string, idx + 1)
 
 # # Same as above just written out.
-
+def isPalindrome(string, idx = 0):
+    j = len(string) -1 - idx
+    if idx >= j:
+        return True
+    if string[idx] != string[j]:
+        return False
+    return isPalindrome(string, i + 1)
 
 string = "racecar"
 print(isPalindrome(string))
