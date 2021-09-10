c27308ad8dde5cb09c0561972a16b3b9294e9ba0
Leland S Crawford <lelandscrawford@gmail.com>
2021-09-10 05:23:31 -0400

    First non-repeating character

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -678,7 +678,13 @@
 
 # print(generateDocument(characters, document))
 # # ---------------------------------------------------------------------------------------------
+def firstNonRepeatingCharacter(string):
+    # Write your code here.
+    return -1
 
+string = abcdcaf
+
+print(FileExistsError(string))
 
 # # ---------------------------------------------------------------------------------------------
 
