135525ace5856a326fbc54406a7a2f6698db975a
Leland S Crawford <lelandscrawford@gmail.com>
2021-09-22 21:58:50 -0400

    LongestPeak-final

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -1122,6 +1122,31 @@ def longestPeak(array):
 array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
 
 print(longestPeak(array))
+# #-------theirs
+def longestPeak(array):
+    longestPeak  = 0
+    i = 1
+    while i < len(array)-1:
+        peak = array[i - 1] < array[i] and array[i + 1] < array[i]
+        if not peak:
+            i += 1
+            continue
+        
+        left = i - 2
+        while left >= 0 and array[left] < array[left + 1]:
+            left -= 1
+        right = i + 2
+        while right < len(array) and array[right] < array[right - 1]:
+            right +=1
+        currentPeak = right - left - 1
+        longestPeak = max(longestPeak, currentPeak)
+        i = right
+                
+    return longestPeak
+
+array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+
+print(longestPeak(array))
 
 # # ---------------------------------------------------------------------------------------------
 
