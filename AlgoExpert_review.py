ae819bf439127870f72d279835f825b2aad66191
Leland S Crawford <lelandscrawford@gmail.com>
2022-07-22 14:21:11 -0400

    July 22, 2022

--- AlgoExpert_review.py
+++ AlgoExpert_review.py
@@ -453,6 +453,27 @@ def main(prog):
 # #-------------------------------------------------------------------------------
 # # 
 
+# # Remove Duplicates From Linked List
+
+# # This is an input class. Do not edit.
+# class LinkedList:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+
+
+# def removeDuplicatesFromLinkedList(linkedList):
+#     currentNode = linkedList
+#     # Write your code here.
+#     while currentNode is not None:
+#         nextNode = currentNode.next
+#         while nextNode and nextNode.value == currentNode.value:
+#             nextNode = nextNode.next
+#         currentNode.next = nextNode
+#         currentNode = nextNode
+        
+#     return linkedList
+
 
 # #-------------------------------------------------------------------------------
 # #  
